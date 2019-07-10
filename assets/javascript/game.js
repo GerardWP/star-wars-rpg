91e1ffdd8284f0850a7a0b08137418d75bbe7503
Gerard Pelly <gerardwp@gmail.com>
2019-07-10 23:09:21 +1000

    html structure, added objects for each avatar

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
 
+    var obiWan = {
+        healthPts: 100,
+        attack: 8,
+        ctrAttack: 8
+    }
+
+    var skywalker = {
+        healthPts: 120,
+        attack: 5,
+        ctrAttack: 5
+    }
+
+    var darthMaul = {
+        healthPts: 180,
+        attack: 25,
+        counterAttack: 25
+    }
+
+    var darthSidious = {
+        healthPts: 150,
+        attack: 20,
+        ctrAttack: 20
+    }
+
+    var topText = $("#top-text");
+    var attackText = $("#attack-stats");
+    var defendText = $("#defend-stats");
+
 
 
 
